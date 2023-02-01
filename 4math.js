d9485bf929bcd084bcc05bdbebbb1c3648bf6fa6
emon3455 <emon.mhk69@gmail.com>
2023-02-02 01:00:02 +0600

    math done

---
+++ 4math.js
@@ -0,0 +1,37 @@
+// power....
+let num1 = Math.pow(2,3);
+console.log(num1);
+
+// absolute vale(that means -5 == 5);
+let num2 = Math.abs(20-40);
+console.log(num2);
+
+// round number:
+let num3  = Math.round(2.3);
+console.log(num3);
+
+// ceiling 
+let num4 = Math.ceil(2.22435);
+console.log(num4);
+
+// floor:
+let num5 = Math.floor(2.87589);
+console.log(num5);
+
+
+// -------------------
+// Random
+// -------------------
+
+console.log(Math.random()); // it will return 0 - 1 in between any number.
+
+let num6 = Math.random()*10;
+console.log(num6);
+
+let num7  = Math.floor(Math.random()*10); //0-9 any number
+console.log(num7);
+
+let num8 = Math.floor(Math.random()*10)+1; //1-10 any number
+console.log(num8);
+
+
