c696f4f7017500e30a2bcdf14ef84847d0f47470
emon3455 <emon.mhk69@gmail.com>
2023-02-01 23:36:27 +0600

    string search done

---
+++ 2search.js
@@ -0,0 +1,9 @@
+// index of:
+let message = "i love only allah";
+let position= message.indexOf("allah"); //return index number if item exist otherwise -1
+console.log(position);
+
+// includes:
+let check = message.includes("love"); //return true is item exist;
+console.log(check);
+
