13510808edffefc57c940aa0f49dbd9464731e1f
emon3455 <emon.mhk69@gmail.com>
2023-02-01 22:52:17 +0600

    string convertion done

---
+++ 1string-convert.js
@@ -0,0 +1,14 @@
+let firstName = "Md Emon";
+let getName  = "md emon";
+
+if( firstName.toLowerCase() === getName.toLowerCase() ){
+    console.log("Valid User");
+}
+else{
+    console.log("Invalid User");
+}
+
+let names = "Emon";
+
+console.log(names.toLowerCase());
+console.log(names.toUpperCase());
